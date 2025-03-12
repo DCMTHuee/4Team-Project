0f37561138342d0b98cf2a1a1608a33990df1804
DCMTHuee <myh000104@naver.com>
2025-03-13 01:12:56 +0900

    시네머신 기능 이동완료
    Ver. 2025.03.13.00001

--- Assets/Project/Scripts/SingletoneBase.cs
+++ Assets/Project/Scripts/SingletoneBase.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using System.Linq;
 
 namespace MoonYoHanStudy
 {
 
     public class SingletoneBase<T> : MonoBehaviour where T : class
     {
+        [Obsolete]
         public static T Singletone
         {
             get
@@ -17,9 +19,11 @@ namespace MoonYoHanStudy
             }
         }
 
+        [Obsolete]
         private static readonly Lazy<T> _Instance = new Lazy<T>(() =>
         {
             T instance = FindObjectOfType(typeof(T)) as T;
+
             if (instance == null)
             {
                 GameObject obj = new GameObject(typeof(T).ToString());
