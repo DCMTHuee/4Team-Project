c5ee58093baa2a24d5869b1f20057100feaa8073
DCMTHuee <myh000104@naver.com>
2025-03-31 12:01:25 +0900

    몬스터 AI 제작
    0.5.1.alpha

---
+++ Assets/z/0_Test/Tree/Scripts/Enemy_Base.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MoonYoHanStudy
+{
+    public class Enemy_Base : MonoBehaviour
+    {
+        internal float MaxHP;
+        internal float MaxST;
+    }
+}
---
+++ Assets/z/0_Test/Tree/Scripts/Enemy_Test1.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+namespace MoonYoHanStudy
+{
+    public enum MonsterType
+    {
+        None,
+        Tree,
+    }
+
+    public enum MonsterState
+    {
+        idle,
+    }
+
+    public class Enemy_Test1 : Enemy_Base
+    {
+        public MonsterState MONSTER_STATE;
+        public MonsterType MONSTER_TYPE;
+
+        [SerializeField] float curHP;
+        [SerializeField] float curST;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            MonsterInit(MONSTER_TYPE);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void MonsterInit(MonsterType monsterType)
+        {
+            switch(monsterType)
+            {
+                case MonsterType.None:
+
+                    MaxHP = 1000;
+                    curHP = MaxHP;
+
+                    MaxST = 100;
+                    curST = MaxST;
+
+                    break;
+
+                case MonsterType.Tree:
+
+                    MaxHP = 2000;
+                    curHP = MaxHP;
+
+                    MaxST = 300;
+                    curST = MaxST;
+
+                    break;
+            }
+        }
+    }
+}
